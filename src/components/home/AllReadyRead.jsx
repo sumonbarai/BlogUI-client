0affe76d5bc4f1f644ecd54a1d7a6bcc1d25ca3c
sumonbarai <sumonbarai78@gmail.com>
2023-01-11 00:28:37 +0600

    history route problem fix

--- src/components/home/AllReadyRead.jsx
+++ src/components/home/AllReadyRead.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import dayCalculator from "../../utilites/dayCalculator";
 
 const AllReadyRead = ({ blog }) => {
-  const { duration, image, timestamp, views, _id } = blog;
+  const { duration, image, timestamp, views, _id, title } = blog;
   return (
     <Link to={`/blog/${_id}`}>
       <div className="card card-side bg-base-100 rounded-none my-2 cursor-pointer">
         <div className="p-4">
-          <h2 className="">New movie is released!</h2>
+          <h2 className="">{title}</h2>
           <div className="text-slate-400">
             <span>{dayCalculator(timestamp)}</span>
             <span className="px-1">.</span>
